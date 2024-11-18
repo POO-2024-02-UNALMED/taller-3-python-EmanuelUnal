5ef6c9b736fa313d986123911975df864495df08
EmanuelUnal <empalaciop@unal.edu.co>
2024-11-18 02:17:34 +0000

    real test 7

--- televisores/tv.py
+++ televisores/tv.py
@@ -20,7 +20,7 @@ class TV:
         return self._canal
 
     def setCanal(self,canal):
-        if self.estado == True and canal >= 1 and canal <= 120:
+        if self._estado == True and canal >= 1 and canal <= 120:
             self._canal = canal
     
     def getPrecio(self):
@@ -33,7 +33,7 @@ class TV:
         return self._volumen
 
     def setVolumen(self,volumen):
-        if self.estado == True and volumen >= 0 and volumen <= 7:
+        if self._estado == True and volumen >= 0 and volumen <= 7:
             self._volumen = volumen
 
     def getControl(self):
